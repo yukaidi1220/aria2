9727591771e44603d2b74a5332c252249ab01c7f
yukaidi <yukaidi@duck.com>
2026-06-08 16:16:37 +0800

    Add HTTP2 single stream transaction

---
+++ src/Http2Transaction.cc
@@ -0,0 +1,93 @@
+/* <!-- copyright */
+/*
+ * aria2 - The high speed download utility
+ *
+ * Copyright (C) 2026 Tatsuhiro Tsujikawa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version of the file(s).  If you delete this exception statement from
+ * all source files in the program, then also delete it here.
+ */
+/* copyright --> */
+#include "Http2Transaction.h"
+
+#ifdef HAVE_LIBNGHTTP2
+
+#  include "DlAbortEx.h"
+#  include "HttpResponse.h"
+
+namespace aria2 {
+
+Http2Transaction::Http2Transaction() = default;
+
+Http2Transaction::~Http2Transaction() = default;
+
+int32_t Http2Transaction::submitRequest(const Http2HeaderBlock& headers)
+{
+  if (streamId_ != 0) {
+    throw DL_ABORT_EX("HTTP/2 transaction already has an active stream");
+  }
+  streamId_ = connection_.submitRequest(headers);
+  return streamId_;
+}
+
+std::string Http2Transaction::drainOutboundData()
+{
+  return connection_.drainOutboundData();
+}
+
+void Http2Transaction::feedInboundData(const std::string& data)
+{
+  connection_.feedInboundData(data);
+}
+
+bool Http2Transaction::hasActiveStream() const { return streamId_ != 0; }
+
+int32_t Http2Transaction::getStreamId() const { return streamId_; }
+
+const Http2ResponseEvent* Http2Transaction::findResponseEvent() const
+{
+  if (streamId_ == 0) {
+    return nullptr;
+  }
+  return connection_.findResponseEvent(streamId_);
+}
+
+std::unique_ptr<HttpResponse> Http2Transaction::popHttpResponse()
+{
+  if (streamId_ == 0) {
+    return nullptr;
+  }
+  auto response = connection_.popHttpResponse(streamId_);
+  if (response) {
+    streamId_ = 0;
+  }
+  return response;
+}
+
+} // namespace aria2
+
+#endif // HAVE_LIBNGHTTP2
---
+++ test/Http2TransactionTest.cc
@@ -0,0 +1,115 @@
+#include "common.h"
+
+#ifdef HAVE_LIBNGHTTP2
+
+#  include "Http2Transaction.h"
+
+#  include <string>
+
+#  include <cppunit/extensions/HelperMacros.h>
+#  include <nghttp2/nghttp2.h>
+
+#  include "DlAbortEx.h"
+#  include "Http2TestUtil.h"
+#  include "HttpHeader.h"
+#  include "HttpResponse.h"
+
+namespace aria2 {
+
+class Http2TransactionTest : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(Http2TransactionTest);
+  CPPUNIT_TEST(testSubmitRequestStoresSingleStreamId);
+  CPPUNIT_TEST(testDrainOutboundDataAfterSubmit);
+  CPPUNIT_TEST(testFeedInboundDataPopHttpResponse);
+  CPPUNIT_TEST(testRejectSecondSubmitWhileStreamActive);
+  CPPUNIT_TEST(testMalformedResponseKeepsActiveStream);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void testSubmitRequestStoresSingleStreamId();
+  void testDrainOutboundDataAfterSubmit();
+  void testFeedInboundDataPopHttpResponse();
+  void testRejectSecondSubmitWhileStreamActive();
+  void testMalformedResponseKeepsActiveStream();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Http2TransactionTest);
+
+void Http2TransactionTest::testSubmitRequestStoresSingleStreamId()
+{
+  Http2Transaction transaction;
+  auto streamId = transaction.submitRequest(http2test::createRequestHeaders());
+
+  CPPUNIT_ASSERT(streamId > 0);
+  CPPUNIT_ASSERT(transaction.hasActiveStream());
+  CPPUNIT_ASSERT_EQUAL(streamId, transaction.getStreamId());
+}
+
+void Http2TransactionTest::testDrainOutboundDataAfterSubmit()
+{
+  Http2Transaction transaction;
+  transaction.submitRequest(http2test::createRequestHeaders());
+
+  auto data = transaction.drainOutboundData();
+
+  CPPUNIT_ASSERT(data.size() > 24);
+  CPPUNIT_ASSERT(http2test::containsFrameType(data, NGHTTP2_SETTINGS));
+  CPPUNIT_ASSERT(http2test::containsFrameType(data, NGHTTP2_HEADERS));
+  CPPUNIT_ASSERT(transaction.drainOutboundData().empty());
+}
+
+void Http2TransactionTest::testFeedInboundDataPopHttpResponse()
+{
+  Http2Transaction transaction;
+  http2test::FakeHttp2ServerSession server;
+  auto streamId = transaction.submitRequest(http2test::createRequestHeaders());
+  auto responseHeaders = http2test::createResponseHeaders();
+  responseHeaders.emplace_back("content-length", "321");
+
+  server.feedInboundData(transaction.drainOutboundData());
+  server.submitResponse(streamId, responseHeaders, "body");
+  transaction.feedInboundData(server.drainOutboundData());
+
+  auto event = transaction.findResponseEvent();
+  CPPUNIT_ASSERT(event);
+  CPPUNIT_ASSERT_EQUAL(streamId, event->streamId);
+
+  auto response = transaction.popHttpResponse();
+  CPPUNIT_ASSERT(response);
+  CPPUNIT_ASSERT_EQUAL(200, response->getStatusCode());
+  CPPUNIT_ASSERT_EQUAL((int64_t)321LL, response->getContentLength());
+  CPPUNIT_ASSERT(!transaction.hasActiveStream());
+  CPPUNIT_ASSERT(!transaction.findResponseEvent());
+  CPPUNIT_ASSERT(!transaction.popHttpResponse());
+}
+
+void Http2TransactionTest::testRejectSecondSubmitWhileStreamActive()
+{
+  Http2Transaction transaction;
+  transaction.submitRequest(http2test::createRequestHeaders());
+
+  CPPUNIT_ASSERT_THROW(
+      transaction.submitRequest(http2test::createRequestHeaders()), DlAbortEx);
+}
+
+void Http2TransactionTest::testMalformedResponseKeepsActiveStream()
+{
+  Http2Transaction transaction;
+  http2test::FakeHttp2ServerSession server;
+  auto streamId = transaction.submitRequest(http2test::createRequestHeaders());
+  Http2HeaderBlock responseHeaders;
+  responseHeaders.emplace_back(":status", "0");
+
+  server.feedInboundData(transaction.drainOutboundData());
+  server.submitResponse(streamId, responseHeaders, "");
+  transaction.feedInboundData(server.drainOutboundData());
+
+  CPPUNIT_ASSERT(transaction.hasActiveStream());
+  CPPUNIT_ASSERT_THROW(transaction.popHttpResponse(), DlAbortEx);
+  CPPUNIT_ASSERT(transaction.hasActiveStream());
+  CPPUNIT_ASSERT(transaction.findResponseEvent());
+}
+
+} // namespace aria2
+
+#endif // HAVE_LIBNGHTTP2
